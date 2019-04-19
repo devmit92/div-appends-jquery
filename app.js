407ebf0a2bd25b5d00749c2412cd2329b56ecef0
Scott Bromander <scottbromander@Scotts-MacBook-Pro.local>
2019-04-19 15:02:06 -0500

    update

--- app.js
+++ app.js
@@ -1,15 +1,5 @@
 // On document ready, console log "something".
-$(document).ready(function () {
-    console.log(peopleArray);
-
-    for (let i = 0; i < peopleArray.length; i++) {
-        $('.container').append('<div></div>');
-        let newDiv = $('.container').children().last();
-        let person = peopleArray[i];
-        newDiv.append('<p>' + person.name + '</p>');
-        newDiv.append('<p>' + person.salary + '</p>');
-    }
-});
+$(document).ready(onReady);
 
 let peopleArray = [];
 
@@ -27,3 +17,46 @@ peopleArray.push({
     name: 'Konou',
     salary: 1000000,
 });
+
+function onReady() {
+    render();
+
+    $('.container').on('click', '.deleteBtn', clickDeleteButton);
+    $('#newEmployee').on('submit', submitForm);
+}
+
+function clickDeleteButton() {
+    let indexForDelete = $(this).parent().data('id');
+    peopleArray.splice(indexForDelete, 1);
+    render();
+}
+
+function submitForm(event) {
+    event.preventDefault();
+
+    let entry = {};
+
+    $(this).serializeArray().forEach(function(item){
+        entry[item.name] = item.value;
+    });
+
+    $(this).trigger('reset');
+
+    //At this point, we can decide what to
+    //do with the object, 'entry'.
+    peopleArray.push(entry);
+    render();
+}
+
+function render() {
+    $('.container').empty();
+    for (let i = 0; i < peopleArray.length; i++) {
+        $('.container').append('<div></div>');
+        let newDiv = $('.container').children().last();
+        newDiv.data('id', i);
+        let person = peopleArray[i];
+        newDiv.append('<p>' + person.name + '</p>');
+        newDiv.append('<p>' + person.salary + '</p>');
+        newDiv.append('<button class="deleteBtn">Fire</button>');
+    }
+}
